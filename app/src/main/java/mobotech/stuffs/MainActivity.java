da0ba65adf1540153008f3ed696a6962f0e2e696
dev-bpn <jazzbpn@gmail.com>
2015-12-31 13:16:31 +0545

    Basic Dialog

--- app/src/main/java/mobotech/stuffs/MainActivity.java
+++ app/src/main/java/mobotech/stuffs/MainActivity.java
@@ -1,9 +1,11 @@
 package mobotech.stuffs;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import com.afollestad.materialdialogs.MaterialDialog;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +13,21 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Basic Dialog
+        basicDialog();
+
+    }
+
+    public void basicDialog(){
+
+        new MaterialDialog.Builder(this)
+                .title("Title")
+                .content("Here goes the content stuffs...")
+                .positiveText("Yes")
+                .negativeText("No")
+                .show();
+
     }
 
     @Override
