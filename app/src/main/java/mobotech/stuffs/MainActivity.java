ef3b3a321a53bd24a5291148b63ec8ca20b61799
dev-bpn <jazzbpn@gmail.com>
2015-12-31 17:05:35 +0545

    ViewPager2 Done

--- app/src/main/java/mobotech/stuffs/MainActivity.java
+++ app/src/main/java/mobotech/stuffs/MainActivity.java
@@ -1,16 +1,44 @@
 package mobotech.stuffs;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import mobotech.stuffs.adapter.MyPagerAdapter;
+import mobotech.stuffs.fragment.MyFragment;
+
 public class MainActivity extends AppCompatActivity {
 
+    MyPagerAdapter pageAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ViewPager pager = (ViewPager)findViewById(R.id.viewpager);
+        List<Fragment> fragments = getFragments();
+        pageAdapter = new MyPagerAdapter(getSupportFragmentManager(), fragments);
+
+        pager.setAdapter(pageAdapter);
+
+    }
+
+    private List<Fragment> getFragments(){
+
+        List<Fragment> fList = new ArrayList<>();
+
+        fList.add(MyFragment.newInstance("Fragment 1"));
+        fList.add(MyFragment.newInstance("Fragment 2"));
+        fList.add(MyFragment.newInstance("Fragment 3"));
+
+        return fList;
+
     }
 
     @Override
---
+++ app/src/main/java/mobotech/stuffs/adapter/MyPagerAdapter.java
@@ -0,0 +1,37 @@
+package mobotech.stuffs.adapter;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.List;
+
+/**
+ * Created by Dell on 12/31/2015.
+ */
+public class MyPagerAdapter extends FragmentPagerAdapter {
+
+    private List<Fragment> fragments;
+
+    public MyPagerAdapter(FragmentManager fm, List<Fragment> fragments) {
+        super(fm);
+        this.fragments = fragments;
+
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        return this.fragments.get(position);
+    }
+
+
+    @Override
+    public int getCount() {
+        return this.fragments.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return "Page: "+ position;
+    }
+}
