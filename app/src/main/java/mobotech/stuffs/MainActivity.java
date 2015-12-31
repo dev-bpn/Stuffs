9810cd7963f087ee7fa489d4fd0fa9b0cdb36e6e
dev-bpn <jazzbpn@gmail.com>
2015-12-31 17:25:58 +0545

    Save Bitmap Image to external Card

--- app/src/main/java/mobotech/stuffs/MainActivity.java
+++ app/src/main/java/mobotech/stuffs/MainActivity.java
@@ -1,9 +1,17 @@
 package mobotech.stuffs;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.os.Environment;
+import android.support.v7.app.AppCompatActivity;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ImageView;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,7 +19,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView imageView1 = (ImageView) findViewById(R.id.imageView);
+        imageView1.setImageResource(R.drawable.my_image);
+
+        BitmapDrawable drawable = (BitmapDrawable) imageView1.getDrawable();
+        Bitmap bitmap = drawable.getBitmap();
+
+        saveImage(bitmap);
+
+
+        ImageView imageView2 = (ImageView) findViewById(R.id.imageView2);
+
+
     }
+
+    private void saveImage(Bitmap finalBitmap) {
+
+        String root = Environment.getExternalStorageDirectory().toString();
+        File myDir = new File(root + "/saved_images");
+        myDir.mkdirs();
+        Random generator = new Random();
+        int n = 10000;
+        n = generator.nextInt(n);
+        String fname = "Image-"+ n +".jpg";
+        File file = new File (myDir, fname);
+        if (file.exists ()) file.delete ();
+        try {
+            FileOutputStream out = new FileOutputStream(file);
+            finalBitmap.compress(Bitmap.CompressFormat.JPEG, 90, out);
+            out.flush();
+            out.close();
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -34,4 +78,5 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
+
 }
