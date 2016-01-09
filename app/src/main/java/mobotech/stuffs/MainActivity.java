0c1412e7a3f92db1758903d81bd28ede072e0832
dev-bpn <jazzbpn@gmail.com>
2016-01-09 12:24:23 +0545

    RudsonMaterialDrawer DONE

--- app/src/main/java/mobotech/stuffs/MainActivity.java
+++ app/src/main/java/mobotech/stuffs/MainActivity.java
@@ -1,17 +1,88 @@
 package mobotech.stuffs;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 
-public class MainActivity extends AppCompatActivity {
+import br.liveo.Model.HelpLiveo;
+import br.liveo.interfaces.OnItemClickListener;
+import br.liveo.interfaces.OnPrepareOptionsMenuLiveo;
+import br.liveo.navigationliveo.NavigationLiveo;
+import mobotech.stuffs.fragment.FragmentMain;
+
+public class MainActivity extends NavigationLiveo implements OnItemClickListener {
+
+    private HelpLiveo mHelpLiveo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+//        setContentView(R.layout.activity_main);
     }
+
+
+    @Override
+    public void onInt(Bundle bundle) {
+
+        this.userName.setText("Rudson Lima");
+        this.userEmail.setText("rudsonlive@gmail.com");
+        this.userPhoto.setImageResource(R.drawable.ic_rudsonlive);
+        this.userBackground.setImageResource(R.drawable.ic_user_background_first);
+
+        // Creating items navigation
+        mHelpLiveo = new HelpLiveo();
+        mHelpLiveo.add(getString(R.string.inbox), R.mipmap.ic_launcher, 7);
+        mHelpLiveo.addSubHeader(getString(R.string.categories)); //Item subHeader
+        mHelpLiveo.add(getString(R.string.starred), R.mipmap.ic_launcher);
+        mHelpLiveo.add(getString(R.string.sent_mail), R.mipmap.ic_launcher);
+        mHelpLiveo.add(getString(R.string.drafts), R.mipmap.ic_launcher);
+        mHelpLiveo.addSeparator(); // Item separator
+        mHelpLiveo.add(getString(R.string.trash), R.mipmap.ic_launcher);
+        mHelpLiveo.add(getString(R.string.spam), R.mipmap.ic_launcher, 120);
+
+        //with(this, Navigation.THEME_DARK). add theme dark
+        //with(this, Navigation.THEME_LIGHT). add theme light
+
+        with(this) // default theme is dark
+                .startingPosition(2) //Starting position in the list
+                .addAllHelpItem(mHelpLiveo.getHelp())
+                .footerItem(R.string.settings, R.mipmap.ic_launcher)
+                .setOnClickUser(onClickPhoto)
+                .setOnPrepareOptionsMenu(onPrepare)
+                .setOnClickFooter(onClickFooter)
+                .build();
+
+    }
+
+
+    @Override //The "R.id.container" should be used in "beginTransaction (). Replace"
+    public void onItemClick(int position) {
+        FragmentManager mFragmentManager = getSupportFragmentManager();
+        Fragment mFragment = new FragmentMain().newInstance(mHelpLiveo.get(position).getName());
+
+        if (mFragment != null){
+            mFragmentManager.beginTransaction().replace(R.id.container, mFragment).commit();
+        }
+    }
+
+
+    private OnPrepareOptionsMenuLiveo onPrepare = new OnPrepareOptionsMenuLiveo() {
+        @Override
+        public void onPrepareOptionsMenu(Menu menu, int position, boolean visible) {
+        }
+    };
+
+
+    private View.OnClickListener onClickPhoto = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            closeDrawer();
+        }
+    };
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -19,6 +90,13 @@ public class MainActivity extends AppCompatActivity {
         getMenuInflater().inflate(R.menu.menu_main, menu);
         return true;
     }
+
+    private View.OnClickListener onClickFooter = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            closeDrawer();
+        }
+    };
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
