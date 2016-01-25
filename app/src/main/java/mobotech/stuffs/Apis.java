f2108d574a1cf619c3be5dbc2b168799e4665e20
dev-bpn <jazzbpn@gmail.com>
2016-01-25 16:34:27 +0545

    DONE ServerError

---
+++ app/src/main/java/mobotech/stuffs/Apis.java
@@ -0,0 +1,20 @@
+package mobotech.stuffs;
+
+/**
+ * Created by Dell on 12/22/2015.
+ */
+public class Apis {
+
+    public static final String BASE_URL = "http://cake.yogeshojha.com.np/";
+    public static final String userDetialPostURl = BASE_URL + "userRegister";
+    public static final String nav_collection = BASE_URL + "collection";
+    public static final String some_categories_list = BASE_URL + "api/child-categories/5?n=3";
+    public static final String some_gift_list = BASE_URL + "api/child-categories/3?n=3";
+    public static final String headerImageSlider = BASE_URL + "api/categories";
+    public static final String urgent_cake = BASE_URL + "api/urgent-cake";
+    public static final String see_all_categories = BASE_URL + "api/child-categories/5";
+    public static final String see_all_gifts = BASE_URL + "api/child-categories/3";
+    public static final String see_all_child_items = BASE_URL + "api/products/66";
+    public static final String defaultImageUrl = "http://www.gettyimages.ca/gi-resources/images/Homepage/Hero/UK/CMS_Creative_164657191_Kingfisher.jpg";
+
+}
--- app/src/main/java/mobotech/stuffs/PostNormalUser.java
+++ app/src/main/java/mobotech/stuffs/PostNormalUser.java
@@ -1,11 +1,8 @@
 package mobotech.stuffs;
 
 import android.app.ProgressDialog;
-import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.android.volley.Request;
 import com.android.volley.Response;
@@ -18,24 +15,20 @@ import org.json.JSONObject;
 import java.util.HashMap;
 import java.util.Map;
 
-public class MainActivity extends AppCompatActivity {
+/**
+ * Created by Dell on 1/19/2016.
+ */
+class PostNormalUserRegister {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
 
-        postUserInfo();
-    }
-
-    private void postUserInfo() {
+    public static void postUserDetials(Context context ) {
 
         // Tag used to cancel the request
         String tag_json_obj = "json_obj_req";
 
         String url = "http://api.androidhive.info/volley/person_object.json";
 
-        final ProgressDialog pDialog = new ProgressDialog(this);
+        final ProgressDialog pDialog = new ProgressDialog(context);
         pDialog.setMessage("Loading...");
         pDialog.show();
 
@@ -74,25 +67,4 @@ public class MainActivity extends AppCompatActivity {
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
